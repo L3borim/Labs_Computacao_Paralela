2d684f7870ce0e4668d4a8cd28e28ec32c1481c3
Ubuntu <ubuntu@ip-172-31-63-127.ec2.internal>
2023-04-25 11:51:04 +0000

    Serial teste 1

---
+++ Projeto/Serial1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long double constanteE(int n){
+  int i, j;
+  long double fat;
+  long double E = 1.0;
+  for(i=1; i<=n; i++){
+    fat = 1;
+    for (j=1; j<=i; j++){
+      fat = fat * j;
+    }
+    E = E + 1.0/fat;
+  } 
+  return E;
+}
+
+
+int main(void) {
+  int n;
+  printf("Digite um número de N para o calculo de E: ");
+  scanf("%d", &n);
+  //n = 5;
+  printf("%.150Lf", constanteE(n));
+  return 0;
+}
