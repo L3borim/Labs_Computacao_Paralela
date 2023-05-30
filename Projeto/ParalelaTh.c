cea5c20a9f24b83a36a1339b10b4ef771d231184
Ubuntu <ubuntu@ip-172-31-92-181.ec2.internal>
2023-05-30 00:05:37 +0000

    Alteração calculo fatorial - Serial e Paralelo

--- Projeto/ParalelaTh.c
+++ Projeto/ParalelaTh.c
@@ -2,49 +2,86 @@
 #include <math.h>
 #include <gmp.h>
 #include <omp.h>
+#include <stdlib.h>
 
 #define NUM_THREADS 2
 
-void constanteE(int n, mpf_t result, int bits) {
-    int i, j;
-    mpf_t fat, termo;
-    mpf_init_set_d(result, 1.0);
-    mpf_init2(fat, bits);
+void constanteE(int n, mpf_t euler, int bits) {
+    int i, tid;
+    mpf_t termo;
     mpf_init2(termo, bits);
+    mpf_set_ui(termo, 1);
 
-    #pragma omp parallel for private(i, j, fat, termo) shared(result) num_threads(NUM_THREADS)
-    for (i = 1; i <= n; i++) {
-        mpf_init2(fat, bits);
-        mpf_init2(termo, bits);
-
-        mpf_set_ui(fat, 1);
-        for (j = 1; j <= i; j++) {
-            mpf_mul_ui(fat, fat, j);
-        }
-        mpf_ui_div(termo, 1, fat);
-
-        #pragma omp critical
-        {
-            mpf_add(result, result, termo);
-        }
-
-        mpf_clear(fat);
-        mpf_clear(termo);
+    mpf_t result_local[NUM_THREADS];
+    for (tid = 0; tid < NUM_THREADS; tid++) {
+        mpf_init2(result_local[tid], bits);
+        mpf_set_ui(result_local[tid], 0);
     }
+
+    #pragma omp parallel private(i, tid) num_threads(NUM_THREADS)
+    {
+        tid = omp_get_thread_num();
+        mpf_t termo_local;
+        mpf_init2(termo_local, bits);
+        mpf_set_ui(termo_local, 1);
+
+        #pragma omp for
+        for (i = 1; i <= n; i++) {
+            mpf_div_ui(termo_local, termo_local, i);
+            mpf_add(result_local[tid], result_local[tid], termo_local);
+        }
+
+        mpf_clear(termo_local);
+    }
+    
+    for (tid = 1; tid < NUM_THREADS; tid++) {
+        mpf_add(result_local[0], result_local[0], result_local[tid]);
+        mpf_clear(result_local[tid]);
+    }
+
+    #pragma omp critical
+    {
+        mpf_add(euler, euler, result_local[0]);
+    }
+
+    mpf_clear(termo);
+    mpf_clear(result_local[0]);
 }
 
 int main() {
-    int n, n_casas;
-    n = 10000;
-    n_casas = 10000;
+    int n = 10000;
+    int n_casas = 10000;
     int bits = ceil(n_casas * log2(10));
-    mpf_set_default_prec(bits);
 
     mpf_t euler;
-    mpf_init(euler);
+    mpf_init2(euler, bits);
+    mpf_set_ui(euler, 1);
 
     constanteE(n, euler, bits);
-    gmp_printf("euler = %.10000Ff\n", euler);
+    gmp_printf("euler para  %d de casas decimais e n = %d\n", n_casas, n);
+    // gmp_printf("euler = %.10000Ff\n", euler);
+    
+    // Abrir o arquivo para escrita
+    
+    FILE *arquivo = fopen("resultado.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    // Converter o valor de euler em uma string
+    int n_casas_decimais = n_casas + 2;  // Número de casas decimais desejadas
+    mp_exp_t exp;
+    char *euler_str = mpf_get_str(NULL, &exp, 10, n_casas_decimais, euler);
+
+    // Gravar a string no arquivo
+    fprintf(arquivo, "%s\n", euler_str);
+
+    // Liberar a memória alocada
+    free(euler_str);
+
+    // Fechar o arquivo
+    fclose(arquivo);
 
     mpf_clear(euler);
     return 0;
--- Projeto/SerialGMP.c
+++ Projeto/SerialGMP.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 #include <gmp.h>
-void constanteE(int n, mpf_t result, int bits) {
-    int i, j;
-    mpf_t fat, termo;
-    mpf_init_set_d(result, 1.0);
-    mpf_init2(fat, bits);
+#include <stdlib.h>
+
+void constanteE(int n, mpf_t *euler, int bits) {
+    int i;
+    mpf_t termo;
+    mpf_set_ui(*euler, 1);
     mpf_init2(termo, bits);
+    mpf_set_ui(termo, 1);
     for (i = 1; i <= n; i++) {
-        mpf_set_ui(fat, 1);
-        for (j = 1; j <= i; j++) {
-            mpf_mul_ui(fat, fat, j);
-        }
-        mpf_ui_div(termo, 1, fat);
-        mpf_add(result, result, termo);
+        mpf_div_ui(termo, termo, i);
+        mpf_add(*euler, *euler, termo);
     }
-    mpf_clear(fat);
     mpf_clear(termo);
 }
 int main() {
     int n, n_casas;
-   // printf("Digite um número de N para o cálculo de E: ");
-   // scanf("%d", &n);
+
     n = 10000;
     n_casas = 10000;
 
@@ -29,10 +25,32 @@ int main() {
     mpf_set_default_prec(bits);
     
     mpf_t euler;
-    mpf_init(euler);
+    mpf_init2(euler, bits);
 
-    constanteE(n, euler, bits);
-    gmp_printf("euler = %.10000Ff\n", euler);
+    constanteE(n, &euler, bits);
+    //gmp_printf("euler = %.100Ff\n", euler);
+    gmp_printf("euler para %d de casas decimais e n = %d\n", n_casas, n);
+   
+    FILE *arquivo = fopen("resultado.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    // Converte o valor de euler em uma string
+    int n_casas_decimais = n_casas + 2;  // Número de casas decimais desejadas
+    mp_exp_t exp;
+    char *euler_str = mpf_get_str(NULL, &exp, 10, n_casas_decimais, euler);
+
+    // Grava a string no arquivo
+    fprintf(arquivo, "%s\n", euler_str);
+
+    // Libera a memória alocada
+    free(euler_str);
+
+    // Fecha o arquivo
+    fclose(arquivo);
+    
 
     mpf_clear(euler);
     return 0;
